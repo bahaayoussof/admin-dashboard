0abca56a1d5294cb02364d668efae27a5ff52421
Bahaa Youssof <bahaayoussof@gmail.com>
2023-01-18 19:51:04 +0200

    finish sidebar content

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,15 @@
+import Sidebar from "../../components/sidebar/Sidebar";
 import "./home.scss";
 
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<div className="home">
+			<Sidebar />
+      <div className="homeContainer">
+        Home Container
+      </div>
+		</div>
+	);
 };
 
 export default Home;
