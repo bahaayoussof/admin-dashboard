f9a4fb140cb67458e084562af1ffb072a2e1dec0
Bahaa Youssof <bahaayoussof@gmail.com>
2023-02-04 16:46:13 +0200

    handle links with react-router-dom

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -12,13 +12,16 @@ import {
 	SettingsSystemDaydreamOutlined,
 	Store,
 } from "@mui/icons-material";
-import "./sidebar.scss";
+import { Link } from "react-router-dom";
 
+import "./sidebar.scss";
 const Sidebar = () => {
 	return (
 		<div className="sidebar">
 			<div className="top">
-				<span className="logo">DashLogo</span>
+				<Link to="/" style={{ textDecoration: "none" }}>
+					<span className="logo">DashLogo</span>
+				</Link>
 			</div>
 
 			<hr />
@@ -31,14 +34,18 @@ const Sidebar = () => {
 						<span>Dashboard</span>
 					</li>
 					<p className="title">LISTS</p>
-					<li>
-						<PersonOutline className="icon" />
-						<span>Users</span>
-					</li>
-					<li>
-						<Store className="icon" />
-						<span>Products</span>
-					</li>
+					<Link to="/users" style={{ textDecoration: "none" }}>
+						<li>
+							<PersonOutline className="icon" />
+							<span>Users</span>
+						</li>
+					</Link>
+					<Link to="/products" style={{ textDecoration: "none" }}>
+						<li>
+							<Store className="icon" />
+							<span>Products</span>
+						</li>
+					</Link>
 					<li>
 						<CreditCard className="icon" />
 						<span>Orders</span>
